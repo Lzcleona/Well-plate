4468b1aa06dbd0be2b54465a4d1c83e0bd53e203
Lzcleona <leonalinzhaocen@gmail.com>
2024-07-02 20:00:49 -0700

    quick change

--- well-plate/src/App.js
+++ well-plate/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/Navbar';
 import Footer from './components/Footer';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Browser router does not currently play nice with GH Pages
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Join from './pages/Join';
